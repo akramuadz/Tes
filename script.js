5f5b2c7c55827903e7de9a027f0b0987cfb5ec08
akramuadz <156046154+akramuadz@users.noreply.github.com>
2025-02-02 15:35:26 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Contoh: Alert saat halaman dimuat
+window.onload = function() {
+    alert("Haii sayang, Ciee udah 17 aja nihh :)) ♡💙");
+};
+
+// Contoh: Menambahkan efek klik ke tombol (jika ada)
+document.querySelector('nav a').addEventListener('click', function() {
+    console.log('Tombol diklik!');
+});
